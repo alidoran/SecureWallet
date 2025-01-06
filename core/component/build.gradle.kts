28e3c3dd5a5325b82e7c9b6022ca41ea87ec696e
alidoran <alidoran@gmail.com>
2025-01-06 09:11:04 -0600

    Add component module

---
+++ core/component/build.gradle.kts
@@ -0,0 +1,72 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.serialization)
+}
+
+android {
+    namespace = "ir.dorantech.component"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 29
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+kotlin {
+    androidTarget {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
+    }
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "ComposeApp"
+            isStatic = true
+        }
+    }
+
+    jvm("desktop")
+
+    sourceSets {
+        androidMain.dependencies {
+            api(compose.preview)
+            api(libs.androidx.activity.compose)
+            implementation(libs.androidx.ui.tooling)
+        }
+        commonMain.dependencies {
+            api(compose.components.resources)
+            api(compose.components.uiToolingPreview)
+            api(compose.foundation)
+            api(compose.material)
+            api(compose.runtime)
+            api(compose.ui)
+        }
+    }
+}
